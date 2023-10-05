b7014cf258551121a6b2adbcf01f8a848d1a46fd
lomakina0612 <lomakina0612@gmail.com>
2023-10-05 19:13:20 +0200

    add phone check

--- homework_4.py
+++ homework_4.py
@@ -1,3 +1,5 @@
+import re
+
 def input_error(func):
     def wrapper(*args, **kwargs):
         try:
@@ -88,6 +90,26 @@ def main():
             print(
                 "Invalid command. Type 'good bye', 'close', 'exit', 'hello', or 'add ...' to interact with the bot."
             )
+            @input_error
+def add_contact(name, phone):
+    # Проверка формата номера и отсутствия лишних символов
+    if re.match(r'^\+380\(\d\d\)\d{3}\-(\d\-\d{3}|\d{2}\-\d{2})$', phone):
+        phonebook[name] = phone
+        return f"Contact {name} with phone number {phone} added."
+    else:
+        return "Invalid phone number format or contains invalid characters. Please use the format +380(xx)xxx-xx-xx or +380(xx)xxx-xxx."
+
+@input_error
+def change_contact(name, phone):
+    if name in phonebook:
+        # Проверка формата номера и отсутствия лишних символов
+        if re.match(r'^\+380\(\d\d\)\d{3}\-(\d\-\d{3}|\d{2}\-\d{2})$', phone):
+            phonebook[name] = phone
+            return f"Phone number for {name} updated."
+        else:
+            return "Invalid phone number format or contains invalid characters. Please use the format +380(xx)xxx-xx-xx or +380(xx)xxx-xxx."
+    else:
+        return f"Contact {name} not found."
 
 
 if __name__ == "__main__":
